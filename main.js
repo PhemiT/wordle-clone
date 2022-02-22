e47b6ac9a1105462fdbd3e92e4fa2e24177d7bfb
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-02-22 09:59:17 +0100

    dataset configuration

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-const lettersGuide = /[a-z]/
+const lettersPattern = /[a-z]/
+let currentGuessCount = 1
+let currentGuess = document.querySelector("#guess" + currentGuessCount)
+//let currentLetters = currentGuess.dataset.letters
 
 document.addEventListener("keydown", (k) => {
     console.log("keypress" + k.key)
-
-
     let keypress = k.key
-    if (keypress.length == 1) {
-        let isLetter = lettersGuide.test(k.key)
-        console.log(isLetter)
+    if (keypress.length == 1 && lettersPattern.test(k.key)) {
+        updateLetters(keypress)
     }
     
 })
+
+ const updateLetters = (letter) => {
+    currentGuess.dataset.letters += letter
+}
+
+const updateTiles = () => {
+
+}
