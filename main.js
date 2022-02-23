b1278769e7264f734245fc8ca925f147367287d9
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-02-23 05:20:50 +0100

    submission configuration

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 const lettersPattern = /[a-z]/
 let currentGuessCount = 1
 let currentGuess = document.querySelector("#guess" + currentGuessCount)
-//let currentLetters = currentGuess.dataset.letters
+const words = ["story", "gnarl", "snail", "zonal", "blair", "metal"]
+let gameWord = ""
+
+const chooseWord = () => {
+    gameWord = words[Math.floor(Math.random()*5)]
+    return gameWord
+}
+
+const submitGuess = () => {
+
+}
 
 document.addEventListener("keydown", (k) => {
     console.log("keypress" + k.key)
     let keypress = k.key
-    if (keypress.length == 1 && lettersPattern.test(k.key)) {
+    if (keypress.length == 1 && lettersPattern.test(k.key) && currentGuess.dataset.letters.length < 5) {
         updateLetters(keypress)
     } else if (k.key == "Backspace" && currentGuess.dataset.letters != "") {
         deleteLetter()
+    } else if (k.key == "Enter" && currentGuess.dataset.letters.length == 5) {
+        submitGuess()
+        for ( let i=0; i < 5; i++) {
+            revealTile(checkLetter(i))
+        }
     }
     
 })
@@ -36,4 +51,24 @@ const deleteLetter = () => {
 
 const clearTile = (tileNumber) => {
     document.querySelector("#guessTile" + tileNumber).innerText = ""
+}
+
+const checkLetter = (position) => {
+    let guessedLetter = currentGuess.dataset.letters.charAt(position)
+    let solutionLetter = gameWord.charAt(position)
+
+    if (solutionLetter == guessedLetter) {
+        return "correct"
+    } else {
+        return checkLetterExists(guessedLetter) ? "present" : "absent"
+    }
+}
+
+const checkLetterExists = (letter) => {
+    return gameWord.includes(letter)
+
+}
+
+const revealTile = (status) => {
+
 }
